6e269a1d62586ddda672d39a9ac264dc618bb16b
codemmk <2107228359@qq.com>
2024-10-28 11:14:14 +0800

    删除重新写

--- 05_learn_redux/src/03_订阅store中的数据.js
+++
@@ -1,9 +0,0 @@
-const store = require('./store')
-
-store.subscribe(() => {
-  console.log(store.getState())
-})
-
-store.dispatch({ type: 'changeName', name: 'milk' })
-store.dispatch({ type: 'changeName', name: 'kobe' })
-store.dispatch({ type: 'changeCount', count: 1000 })
--- 06_react_redux/src/store/constants.js
+++
@@ -1,4 +0,0 @@
-export const ADD_NUMBER_TYPE = 'add_number'
-export const SUB_NUMBER_TYPE = 'sub_number'
-export const RECOMMENDS_TYPE = 'recommends'
-export const BANNERS_TYPE = 'banners'
